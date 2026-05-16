ae2a837fa0f8eed21541e4e4ff51aad8a048f0d5
Celeste Capece <celedev97@gmail.com>
2026-05-16 16:48:07 +0200

    aggiunto supporto mdx e cambiata durata animazioni

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,9 @@
 import { defineConfig } from 'astro/config';
+import mdx from '@astrojs/mdx';
 
 export default defineConfig({
   site: 'https://cele.dev',
+  integrations: [mdx()],
   markdown: {
     syntaxHighlight: 'prism',
   },
